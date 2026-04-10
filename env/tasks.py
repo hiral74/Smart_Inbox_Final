e4de838122f3df3e437e3354ac74c2971e3f5892
Hiral <132593143+hiral74@users.noreply.github.com>
2026-04-10 15:41:04 +0530

    Add files via upload

---
+++ env/tasks.py
@@ -0,0 +1,57 @@
+from typing import Dict, Any
+from .models import EmailAction
+
+
+# -----------------------------
+# EASY TASK -- Did the AI correctly understand the email?”
+# -----------------------------
+class EasyTask:
+    name = "easy_classification"
+
+    def grade(self, action: EmailAction, ground_truth: Dict[str, Any]) -> float:
+        if action.classification == ground_truth["classification"]:
+            return 1.0
+        return 0.0
+
+
+# -----------------------------
+# MEDIUM TASK -- Did the AI correctly understand the email + suggest an appropriate action?”
+# -----------------------------
+class MediumTask:
+    name = "medium_classification_action"
+
+    def grade(self, action: EmailAction, ground_truth: Dict[str, Any]) -> float:
+        score = 0.0
+
+        if action.classification == ground_truth["classification"]:
+            score += 0.5
+
+        if action.action == ground_truth["action"]:
+            score += 0.5
+
+        return score
+
+
+# -----------------------------
+# HARD TASK -- Did the AI correctly understand the email + suggest an appropriate action + provide a high-quality response?
+# -----------------------------
+class HardTask:
+    name = "hard_full_email_handling"
+
+    def grade(self, action: EmailAction, ground_truth: Dict[str, Any]) -> float:
+        score = 0.0
+
+        # Classification
+        if action.classification == ground_truth["classification"]:
+            score += 0.3
+
+        # Action
+        if action.action == ground_truth["action"]:
+            score += 0.3
+
+        # Response quality 
+        if ground_truth["action"] == "reply":
+            if action.response and len(action.response) > 15:
+                score += 0.4
+
+        return score
