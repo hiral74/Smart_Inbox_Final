0bc2db588f4c8547586686a8bce54fa99723e529
Hiral <132593143+hiral74@users.noreply.github.com>
2026-04-10 18:18:46 +0530

    Update tasks.py

--- env/tasks.py
+++ env/tasks.py
@@ -10,8 +10,8 @@ class EasyTask:
 
     def grade(self, action: EmailAction, ground_truth: Dict[str, Any]) -> float:
         if action.classification == ground_truth["classification"]:
-            return 1
-        return 0
+            return 0.95
+        return 0.05
 
 
 # -----------------------------
@@ -21,14 +21,15 @@ class MediumTask:
     name = "medium_classification_action"
 
     def grade(self, action: EmailAction, ground_truth: Dict[str, Any]) -> float:
-        score = 0
+        score = 0.0
 
         if action.classification == ground_truth["classification"]:
             score += 0.5
 
         if action.action == ground_truth["action"]:
             score += 0.5
-
+            
+        score=max(0.05,min(0.95,score))
         return score
 
 
@@ -39,7 +40,7 @@ class HardTask:
     name = "hard_full_email_handling"
 
     def grade(self, action: EmailAction, ground_truth: Dict[str, Any]) -> float:
-        score = 0
+        score = 0.0
 
         # Classification
         if action.classification == ground_truth["classification"]:
@@ -53,5 +54,5 @@ class HardTask:
         if ground_truth["action"] == "reply":
             if action.response and len(action.response) > 15:
                 score += 0.4
-
+        score=max(0.05,min(0.95,score))
         return score
